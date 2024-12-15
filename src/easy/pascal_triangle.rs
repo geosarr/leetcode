9cde617da94e9c22a75a4fc7e3b227691c14266f
geosarr <georgesmbissanes@gmail.com>
2024-12-15 01:21:02 +0100

    Added Pascal's Triangle.

---
+++ src/easy/pascal_triangle.rs
@@ -0,0 +1,17 @@
+pub fn generate(n: i32) -> Vec<Vec<i32>> {
+    if n == 1 {
+        return vec![vec![1]];
+    }
+    if n == 2 {
+        return vec![vec![1], vec![1, 1]];
+    }
+    let mut new = vec![0; n as usize];
+    new[0] = 1;
+    new[n as usize - 1] = 1;
+    let mut old = generate(n - 1);
+    for k in 1..(n as usize - 1) {
+        new[k] = old.last().as_ref().unwrap()[k] + old.last().as_ref().unwrap()[k - 1];
+    }
+    old.extend_from_slice(&[new]);
+    return old;
+}
